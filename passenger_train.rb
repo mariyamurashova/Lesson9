814c26f867ff302ca09c352aa5b5d42190fddd69
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-04-19 15:43:53 +0200

    Update passenger_train.rb

--- passenger_train.rb
+++ passenger_train.rb
@@ -2,10 +2,13 @@
 
 class PassengerTrain < Train
   
+  validate :number, :format, TRAIN_NUMBER
+  validate :number, :presence
+
   PassengerTrain.set_instances
   PassengerTrain.set_register_instances
 
-  def initialize(name)
+  def initialize(number)
     super
     @train_type = :passenger
   end
