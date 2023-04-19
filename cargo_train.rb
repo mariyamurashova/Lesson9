23e5ae19587c19fe978e0f1c7ad6e9b133dfcdfd
mariyamurashova <127765865+mariyamurashova@users.noreply.github.com>
2023-04-19 15:43:51 +0200

    Update cargo_train.rb

--- cargo_train.rb
+++ cargo_train.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class CargoTrain < Train
+
+  validate :number, :format, TRAIN_NUMBER
+  validate :number, :presence
+
   CargoTrain.set_instances
   CargoTrain.set_register_instances
 
